c3fab7a20a455c8e013a5dd253760ed920af46d8
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-20 00:15:25 +0400

    Add files via upload

---
+++ cartask 20.12.22/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cartask_20._12._22
+{
+    internal class Car
+    {
+        public string Brand;
+        public string Model;
+        public int FuelFor1Km;
+        public int Millage;
+        public int CurrentFuel;
+        public void Drive()
+        {
+            Console.WriteLine("getmek istediyiniz km qeyd edin");
+            int getmekistenilenyol = Convert.ToInt32(Console.ReadLine());
+            //əgər benzin kifayət edirsə
+          
+            int lazimibenzin = getmekistenilenyol * FuelFor1Km; 
+            if (lazimibenzin <= CurrentFuel)
+            //maşının millage dəyəri və benzini azalır,
+            {
+                CurrentFuel-= FuelFor1Km*getmekistenilenyol;//**getmek istenilen yol gedilikden sonra ne qeder benzon qalirsa onu tapmis olura,
+                Millage+= getmekistenilenyol;
+                Console.WriteLine("benzin kifayet edir");
+                Console.WriteLine(CurrentFuel);
+                Console.WriteLine(Millage);
+            }
+            else
+                Console.WriteLine("benzin kifayet etmir");
+            
+
+        }
+    }
+}
+
---
+++ cartask 20.12.22/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace cartask_20._12._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //car task
+            Car car = new Car
+            {
+                Brand = "Bentley",
+                Model = "Bentayga",
+                FuelFor1Km = 2,// masinin 1 km ucun xerclediyi benzin
+                Millage =20,// Masinin bu vaxtadək neçə km sürüldüyü
+                CurrentFuel = 80 //masinin hazirki benzin miqdari
+            };
+
+            car.Drive();
+
+
+            
+
+
+
+
+        }
+
+    }
+}
+
